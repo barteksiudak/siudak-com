16a38d70aaef1aa96d0fef147bbbc4b80797e490
Bartosz Siudak <bartosz.siudak@idego.io>
2026-02-17 19:39:18 +0100

    added docker-compose

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withNextIntl = require('next-intl/plugin')('./i18n/request.ts');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // SSR mode - no static export
+  output: 'standalone',
 
   // Image optimization
   images: {
